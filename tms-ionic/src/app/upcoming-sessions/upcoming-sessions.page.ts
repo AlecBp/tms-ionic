f674c8bbcd4530f9dff8b66200b07cac6ecf80ac
Alec Pagliarussi <alec.paglia@gmail.com>
2021-03-28 22:03:56 -0400

    better version

--- tms-ionic/src/app/upcoming-sessions/upcoming-sessions.page.ts
+++ tms-ionic/src/app/upcoming-sessions/upcoming-sessions.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface Session {
   date: string;
   time: string;
-  subejcts: string[];
+  subjects: string[];
   location: string;
 }
 @Component({
@@ -25,7 +25,7 @@ export class UpcomingSessionsPage implements OnInit {
     const dummySessions: Session = {
       date: 'Tue - Nov 10',
       time: '4:00PM - 6:00PM',
-      subejcts: ['Math', 'English'],
+      subjects: ['Math', 'English'],
       location: 'George Brown College',
     };
 
