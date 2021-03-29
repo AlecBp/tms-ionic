78aad26c8655c3e970a80bee65d6903c99346bc1
Vincent-hide <hidehiro.okayama@gmail.com>
2021-03-28 23:06:25 -0400

    toast added

--- tms-ionic/src/app/session/session.page.ts
+++ tms-ionic/src/app/session/session.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-session',
@@ -9,7 +10,7 @@ export class SessionPage implements OnInit {
   tutor: string;
   students: string[];
 
-  constructor() {
+  constructor(public toastController: ToastController) {
     this.tutor= "John Doe";
     this.students = [];
     this.loadStudents();
@@ -22,4 +23,15 @@ export class SessionPage implements OnInit {
       this.students.push("Abbey Edward")
     }
   }
+
+  async handleNoteSubmit() {
+    const toast = await this.toastController.create({
+      message: 'a note about the session has been saved',
+      duration: 2000,
+      color: "success",
+      position: "top"
+    });
+    toast.present();
+  }
+
 }
