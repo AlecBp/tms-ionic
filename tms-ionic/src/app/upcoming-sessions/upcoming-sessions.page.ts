ae88254905669bcf7e163a2eabc2f30ec02d314e
Vincent-hide <hidehiro.okayama@gmail.com>
2021-03-28 21:40:42 -0400

    upcommig sessions

--- tms-ionic/src/app/upcoming-sessions/upcoming-sessions.page.ts
+++ tms-ionic/src/app/upcoming-sessions/upcoming-sessions.page.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Session {
+  date: string;
+  time: string;
+  subejcts: string[];
+  location: string;
+}
 @Component({
   selector: 'app-upcoming-sessions',
   templateUrl: './upcoming-sessions.page.html',
   styleUrls: ['./upcoming-sessions.page.scss'],
 })
 export class UpcomingSessionsPage implements OnInit {
+  upcomingSessions: Session[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.upcomingSessions = [];
+    this.loadUpcomingSessions();
   }
 
+  ngOnInit() {}
+
+  loadUpcomingSessions(): void {
+    const dummySessions: Session = {
+      date: 'Tue - Nov 10',
+      time: '4:00PM - 6:00PM',
+      subejcts: ['Math', 'English'],
+      location: 'George Brown College',
+    };
+
+    for (let i = 0; i < 10; i++) {
+      this.upcomingSessions.push(dummySessions);
+    }
+  }
+
+  debugger(val: any): void {
+    console.log(val);
+  }
 }
