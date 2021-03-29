7ba62de8805f9aaebbf4309d7ee76e66acc722fe
Alec Pagliarussi <alec.paglia@gmail.com>
2021-03-28 23:26:12 -0400

    dummy sessions

--- tms-ionic/src/app/upcoming-sessions/upcoming-sessions.page.ts
+++ tms-ionic/src/app/upcoming-sessions/upcoming-sessions.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 
 export interface Session {
+  id: number;
   date: string;
   time: string;
   subjects: string[];
@@ -17,24 +18,74 @@ export class UpcomingSessionsPage implements OnInit {
   upcomingSessions: Session[];
 
   constructor(public toastController: ToastController) {
-    this.upcomingSessions = [];
-    this.loadUpcomingSessions();
+    this.upcomingSessions = [
+      {
+        id: 1,
+        date: 'Tue - Mar 30',
+        time: '5:00PM - 6:00PM',
+        subjects: ['Math', 'English'],
+        location: 'George Brown College',
+      },
+      {
+        id: 2,
+        date: 'Tue - Apr 6',
+        time: '4:00PM - 6:00PM',
+        subjects: ['Math'],
+        location: 'George Brown College',
+      },
+      {
+        id: 3,
+        date: 'Tue - Apr 13',
+        time: '4:00PM - 5:00PM',
+        subjects: ['English'],
+        location: 'George Brown College',
+      },
+      {
+        id: 4,
+        date: 'Tue - Apr 20',
+        time: '4:00PM - 6:00PM',
+        subjects: ['Science'],
+        location: 'George Brown College',
+      },
+      {
+        id: 5,
+        date: 'Tue - Apr 27',
+        time: '5:00PM - 6:00PM',
+        subjects: ['French', 'English'],
+        location: 'George Brown College',
+      },
+      {
+        id: 6,
+        date: 'Tue - May 4',
+        time: '4:00PM - 6:00PM',
+        subjects: ['Math', 'English'],
+        location: 'George Brown College',
+      },
+      {
+        id: 7,
+        date: 'Tue - May 11',
+        time: '4:00PM - 5:00PM',
+        subjects: ['Math'],
+        location: 'George Brown College',
+      },
+    ];
+    // this.loadUpcomingSessions();
   }
 
   ngOnInit() {}
 
-  loadUpcomingSessions(): void {
-    const dummySessions: Session = {
-      date: 'Tue - Nov 10',
-      time: '4:00PM - 6:00PM',
-      subjects: ['Math', 'English'],
-      location: 'George Brown College',
-    };
+  // loadUpcomingSessions(): void {
+  //   const dummySessions: Session = {
+  //     date: 'Tue - Nov 10',
+  //     time: '4:00PM - 6:00PM',
+  //     subjects: ['Math', 'English'],
+  //     location: 'George Brown College',
+  //   };
 
-    for (let i = 0; i < 10; i++) {
-      this.upcomingSessions.push(dummySessions);
-    }
-  }
+  //   for (let i = 0; i < 10; i++) {
+  //     this.upcomingSessions.push(dummySessions);
+  //   }
+  // }
 
   debugger(val: any): void {
     console.log(val);
@@ -44,8 +95,8 @@ export class UpcomingSessionsPage implements OnInit {
     const toast = await this.toastController.create({
       message: 'Successfully Logged Out',
       duration: 2000,
-      color: "success",
-      position: "top"
+      color: 'success',
+      position: 'top',
     });
     toast.present();
   }
